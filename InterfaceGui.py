68e4d94fa33ae0208aac0d960bcb4977d384cd06
ahaine <77943398+ahaine@users.noreply.github.com>
2022-04-07 18:04:23 -0500

    Create InterfaceGui.py
    This python file is for the Interface. Created a basic GUI for the OSINT Interface Application

---
+++ InterfaceGui.py
@@ -0,0 +1,19 @@
+#import * from tkinter to develop GUI
+from tkinter import *
+
+#create a window for GUI
+#instantiate an instance of WinD
+winD = Tk();
+#change the size of winD, using geometry function
+winD.geometry("1280x768");
+#create title for window frame, WinD
+winD.title("OSINT Project");
+#change background of window frame
+winD.config(background="#8c1414");
+
+#display window frame on screen
+winD.mainloop();
+
+
+
+
