554c04b6b2c21935474baad34565dc4646e3cb18
SingerDavid <dsinger1@hawk.iit.edu>
2022-04-28 02:37:40 -0500

    Complete reddit api data scrape and interface

--- InterfaceGui.py
+++ InterfaceGui.py
@@ -4,6 +4,7 @@ from tkinter import *
 from xml.sax.xmlreader import InputSource
 import twint
 import pandas as pd
+import requests
 #To install twint:
     #pip3 install twint
     #pip3 install --user --upgrade git+https://github.com/twintproject/twint.git@origin/master#egg=twint
@@ -19,12 +20,83 @@ winD.title("OSINT Project");
 #change background of window frame
 winD.config(background="#8c1414");
 
+def RedditCrawlClick():
+    #generate label
+    redditlabel = Label(winD, text="Enter your favorite subreddit:")
+    redditlabel.pack()
+    #generate entry
+    redditentry = Entry(winD)
+    redditentry.pack()
+
+    def RedditStart():
+        subredditResult = redditentry.get()
+        #Reddit API, Secrets Keys for David Singer (hard coded them so it works for us all)
+        CLIENT_ID = 'Z67JznUGNKehngQw6zsxxg'
+        SECRETS_KEY = 'CNIwnX7gon7lqWYSVsorJZupN-rt1A'
+
+        auth = requests.auth.HTTPBasicAuth(CLIENT_ID, SECRETS_KEY)
+
+        #need to log in to reddit account that has access. - normally we'd hide this in a .gitignore file and pull it locally, but who cares for this accnt.
+        data = {
+            'grant_type': 'password',
+            'username': 'MaintenanceNeither16',
+            'password': 'OSINTProject'
+        }
+
+        #basically naming this api and the version of the "app"
+        headers = {'User-Agent': 'MyApi/0.0.1'}
+
+        #send request for auth token
+        res = requests.post('https://www.reddit.com/api/v1/access_token',
+                            auth=auth, data=data, headers=headers)
+        TOKEN = res.json()['access_token']
+        print(TOKEN)
+
+        #this token is something we have to add to the header whenever we use the api (ps: ** doesnt really do anything)
+        headers = {**headers, **{'Authorization': f'bearer {TOKEN}'}}
+        print(headers)
+
+        #now we can make requests: this checks to see if we get a return of "200", meaning everything is okay - now add .json and we get a ton of info
+        #this is NOT needed, just a test
+        #requests.get('https://outh.reddit.com/api/v1/me', headers=headers)
+
+        #lets grab subredit, replace hot with new to get new posts, add params to set parameters: params={'limit': '49'} = 50 posts
+        res = requests.get(f'https://oauth.reddit.com/r/{subredditResult}/hot', headers=headers, params={'limit': '49'})
+        res.json()
+
+        #make empty data frame
+        dfReddit = pd.DataFrame()
+
+        #loop posts
+        for post in res.json()['data']['children']:
+            dfReddit = dfReddit.append({
+                        'subreddit': post['data']['subreddit'],
+                        'title':  post['data']['title'],
+                        'selftext': post['data']['selftext'],
+                        'upvote_ratio': post['data']['upvote_ratio'],
+                        'ups': post['data']['ups'],
+                        'downs': post['data']['downs'],
+                        'score': post['data']['score']
+            }, ignore_index=True)
+
+        dfReddit.to_csv(f'{subredditResult}Reddit.csv')
+
+    #frame to house enter button
+    redditFrame = LabelFrame(winD, padx=5, pady=5)
+    redditFrame.pack(padx=10, pady=10)
+
+    #scrape subreddits button
+    topicbutton = Button(redditFrame, text="Get Reddit Data", padx=50, command=RedditStart)
+    topicbutton.grid()
+
+#end of reddit API
+
 #create frame to house buttons
 buttonframe = LabelFrame(winD, padx=5, pady=5)
 buttonframe.pack(padx=10, pady=10)
 
-#Facebook button
-redditbutton = Button(buttonframe, text="Reddit", padx=60, pady=40, bg="#F28C28")
+#Reddit button
+redditbutton = Button(buttonframe, text="Reddit", padx=60, pady=40, bg="#F28C28", command=RedditCrawlClick)
 redditbutton.grid()
 
 #onclick to web scrape twitter
@@ -62,7 +134,7 @@ def twittercrawlclick():
 twitterbutton = Button(buttonframe, text="Twitter", padx=60, pady=40, bg="blue", command=twittercrawlclick)
 twitterbutton.grid()
 
-#onclick function to open input 
+#onclick function to open input
 def webcrawlclick():
     crawllabel = Label(winD, text="Please enter web address:")
     crawllabel.pack()
